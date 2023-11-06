74e41bf828660be57afb67a117c72cb7485b9cf4
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-11-06 14:57:16 +0300

    bot Details  styling

--- bot-battlr-app/src/components/BotSpecs.jsx
+++ bot-battlr-app/src/components/BotSpecs.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
-
+import './../styles/botSpecs.css'
 function BotSpecs({ bot, onEnlistBot, onBack }) {
       return (
         <div className='bot-specs'>
           <h2>Bot Details</h2>
           <div className='specs'>
             <div className='image'>
-            <img className='avatar' src={bot.avatar_url} alt={bot.name} />
+            <img className='avatarr' src={bot.avatar_url} alt={bot.name} />
             </div>
-            <div className='botInfo'></div>
-            <h3>{bot.name}</h3>
-            <p>{bot.catchphrase}</p>
-            <p>🩶{bot.health} | 🪖{bot.damage} | 🛡️{bot.armor}</p>
-            <button onClick={() => onEnlistBot(bot)}>Enlist</button>
-            <button onClick={onBack}>Back to List</button>
+            <div className='botInfo'>
+            <h5>{bot.name}</h5>
+            <p className='bot-inffo'>Bot Class:    {bot.bot_class}</p>
+            <p className='bot-inffo'>Catchphrase:</p>
+            <p className='bot-inffo'>{bot.catchphrase}</p>
+            <p className='bttn'>🩶{bot.health}   |   🪖{bot.damage}   |   🛡️{bot.armor}</p>
+            <button className='btnn' onClick={() => onEnlistBot(bot)}>Enlist</button>
+            <button className='btnn' onClick={onBack}>Back to List</button>
+
+            </div>
+            
           </div>
         </div>
       );
